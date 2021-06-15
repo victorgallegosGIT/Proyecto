d7a8f9efcf9d7f5c1b343104f6d131b97c8ececc
Alejo Carbonari <crowdar.acarbonari@veritran.com>
2021-06-15 19:06:06 -0300

    migración a MSQL

--- TiendaOnline/TiendaOnline/settings.py
+++ TiendaOnline/TiendaOnline/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'TiendaOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'tiendaonline',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+            'OPTIONS': {
+                'sql_mode': 'traditional',
+            }
     }
 }
 
